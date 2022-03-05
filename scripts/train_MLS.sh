51c591e5f29ca9bd1171a81afc36008c6ec0c0eb
Chen Liang <547205480@qq.com>
2022-03-05 15:07:40 +0800

    add iwslt14

--- scripts/train_MLS.sh
+++ scripts/train_MLS.sh
@@ -1,9 +1,9 @@
-export CUDA_VISIBLE_DEVICES=0
-fairseq-train ../databin/wmt16-ro-en-joined \
---save-dir ../checkpoints/ro-en-MLS-0.1 \
+export CUDA_VISIBLE_DEVICES=2
+fairseq-train ../databin/iwslt14-de-en-joined-new \
+--save-dir ../checkpoints/de-en-MLS-0.1 \
 --arch transformer_wmt_en_de \
 --optimizer adam --adam-betas '(0.9, 0.98)' \
---lr-scheduler inverse_sqrt --warmup-init-lr 1e-07 --warmup-updates 2000 \
+--lr-scheduler inverse_sqrt --warmup-init-lr 1e-07 --warmup-updates 1000 \
 --lr 0.0007 \
 --eval-bleu \
 --eval-bleu-args '{"beam": 5, "max_len_a": 1.2, "max_len_b": 10}' \
@@ -13,11 +13,11 @@ fairseq-train ../databin/wmt16-ro-en-joined \
 --criterion masked_label_smoothed_cross_entropy \
 --label-smoothing 0.1 --dropout 0.3 --weight-decay 0.0001 \
 --max-tokens  2048  \
---update-freq 8 \
+--update-freq 4 \
 --no-progress-bar --log-format json --log-interval 500 \
---max-epoch 50 \
---keep-last-epochs 2 \
+--max-epoch 20 \
+--keep-last-epochs 3 \
 --fp16 \
 --seed 666 \
---joint-vocab-dir ../databin/wmt16-ro-en-joined/dict.en.txt \
---tgt-vocab-dir ../databin/wmt16-ro-en-isolated/dict.en.txt       
+--joint-vocab-dir  ../databin/iwslt14-de-en-joined-new/dict.de.txt \
+--tgt-vocab-dir ../databin/iwslt14-de-en-isolated-new/dict.en.txt     
